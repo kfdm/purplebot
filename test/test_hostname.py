4d1faa781b4e4a7ec45dad9c42a4f999841c04fe
Paul Traylor <kungfudiscomonkey@gmail.com>
2012-09-22 14:10:33 -0700

    Hostmask test

---
+++ test/test_hostname.py
@@ -0,0 +1,20 @@
+try:
+    import unittest2 as unittest
+except ImportError:
+    import unittest
+
+from purplebot.test import testbot
+
+
+class TestHostname(unittest.TestCase):
+    def setUp(self):
+        self.bot = testbot()
+
+    def test_invalid_hostname(self):
+        with self.assertRaises(IndexError):
+            self.bot.parse_hostmask('foo')
+
+    def test_hostname(self):
+        nick, mask = self.bot.parse_hostmask(':KFDM!~paul@purple')
+        self.assertEqual(nick, 'KFDM')
+        self.assertEqual(mask, '~paul@purple')
