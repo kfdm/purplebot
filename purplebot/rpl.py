35ca5106e830fa5798e2c6689d3aba192143418a
Paul Traylor <kungfudiscomonkey@gmail.com>
2013-02-20 22:39:41 -0800

    Support dropping into the python interpreter

--- purplebot/rpl.py
+++ purplebot/rpl.py
@@ -1,24 +1,38 @@
 import cmd
 import logging
+import code
 from purplebot.test import testbot
 
 
 class RPL(cmd.Cmd):
-    def do_init(self, *args):
+    prompt = 'bot> '
+
+    def do_spawn(self, *args):
+        """Spawn new bot"""
         self.bot = testbot()
         self.bot.connect('localhost', 6667, 'testbot', 'testbot', 'testbot')
 
     def do_exit(self, line):
-        exit(0)
+        """Exit RPL"""
+        return True
+
+    def do_shell(self, line):
+        """Jump into a Python Shell"""
+        code.interact(local={'bot': self.bot})
+
+    def emptyline(self):
+        pass
 
     def default(self, line):
+        if line == 'EOF':
+            return True
         self.bot._parse_line(line)
 
 
 def main():
     logging.basicConfig(level=logging.DEBUG)
     cli = RPL()
-    cli.do_init()
+    cli.do_spawn()
     cli.cmdloop()
 
 if __name__ == '__main__':
